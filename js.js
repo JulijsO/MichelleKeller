7c153d035d1bdb5100c5d3cfe1e3b2c1babed288
JulijsO <77743919+JulijsO@users.noreply.github.com>
2021-02-01 13:52:56 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,176 @@
+$(window).on("scroll touchmove", function () {
+            if ($(document).scrollTop() >= -0 + $('#img1').position().top && $(document).scrollTop() < $('#img2').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img2').position().top && $(document).scrollTop() < $('#img3').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M2.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img3').position().top && $(document).scrollTop() < $('#img4').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M3.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img4').position().top && $(document).scrollTop() < $('#img5').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M4.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img5').position().top && $(document).scrollTop() < $('#img6').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M5.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img6').position().top && $(document).scrollTop() < $('#img7').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M6.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img7').position().top && $(document).scrollTop() < $('#img8').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M7.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img8').position().top && $(document).scrollTop() < $('#img9').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M8.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img9').position().top && $(document).scrollTop() < $('#img10').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M9.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img10').position().top && $(document).scrollTop() < $('#img11').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M10.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img11').position().top && $(document).scrollTop() < $('#img12').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M11.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img12').position().top && $(document).scrollTop() < $('#img13').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M12.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img13').position().top && $(document).scrollTop() < $('#img14').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M13.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img14').position().top && $(document).scrollTop() < $('#img15').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M14.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img15').position().top && $(document).scrollTop() < $('#img16').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M15.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img16').position().top && $(document).scrollTop() < $('#img17').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M16.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img17').position().top && $(document).scrollTop() < $('#img18').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M17.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img18').position().top && $(document).scrollTop() < $('#img19').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M18.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img19').position().top && $(document).scrollTop() < $('#img20').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M19.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img20').position().top && $(document).scrollTop() < $('#img21').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M20.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img21').position().top && $(document).scrollTop() < $('#img22').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M21.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img22').position().top && $(document).scrollTop() < $('#img23').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M22.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img23').position().top && $(document).scrollTop() < $('#img24').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M23.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img24').position().top && $(document).scrollTop() < $('#img25').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M24.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img25').position().top && $(document).scrollTop() < $('#img26').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M25.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img26').position().top && $(document).scrollTop() < $('#img27').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M26.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img27').position().top && $(document).scrollTop() < $('#img28').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M27.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img28').position().top && $(document).scrollTop() < $('#img29').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M28.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img29').position().top && $(document).scrollTop() < $('#img30').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M29.jpg)')
+            };
+            
+
+            if ($(document).scrollTop() >= -100 + $('#img30').position().top && $(document).scrollTop() < $('#img31').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M30.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -0 + $('#img31').position().top && $(document).scrollTop() < $('#img32').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M31.jpg)')
+            };
+
+
+            if ($(document).scrollTop() >= -00 + $('#img32').position().top && $(document).scrollTop() < $('#imgPH').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M32.jpg)')
+            };
+
+
+            // if ($(document).scrollTop() >= -00 + $('#imgPH').position().top && $(document).scrollTop() < $('#img20').position().top) {
+            //     $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M32.jpg)')
+            // };
+
+
+            if ($(document).scrollTop() >= $('#imgPH').position().top) {
+                $('body').css('background-image', 'url(img/Patterns_BG/Pattern_M_PH.png)')
+        };
+        });
+
+
+
+
+
+
